9b5b97986e67bed8c2cec14ab5c5fbb7f247b5b6
Linka <polina153@mail.ru>
2022-05-02 22:57:28 +0400

    initial commit

---
+++ app/src/main/java/ru/geekbrains/mycalculator/MainActivity.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.mycalculator;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("ButtonClick", "Button 0 clicked");
+                int id = view.getId();
+                view.setVisibility(View.GONE);
+                int name = view.getVisibility();
+                int name2 = view.getVisibility();
+            }
+        });
+    }
+}
